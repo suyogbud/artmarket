2304dc1496d7a2d118bea45adcea4975bf9b8c23
Suyog <suyogbc10@gmail.com>
2024-04-15 23:59:44 -0400

    Added click functions to navitema

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,10 +1,35 @@
 "use client";
+import { PRODUCT_CATEGORIES } from "@/app/config";
 import React, { useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-  return <div className="flex gap-4 h-full">NavItems</div>;
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+        const isOpen = activeIndex === i;
+        const isAnyOpen = activeIndex !== null;
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
