428a01ebea51f014c31fb6a04ffc3709776e22f2
Suyog Singh Budhathoki <suyogbc10@gmail.com>
2024-05-14 13:36:20 -0400

    Revert "Added Email verification logic [timestamp: 2024-05-03 11:33:42]"
    This reverts commit 71e5f62d7350952c34887dbe701efa7ed0b06441.

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,7 +2,6 @@ import { schemaValidator } from "../lib/validators/account-credentials-validator
 import { publicProcedure, router } from "./trpc";
 import { getPayloadClient } from "../get-payload";
 import { TRPCError } from "@trpc/server";
-import { z } from "zod";
 export const authRouter = router({
   createPayloadUser: publicProcedure
     .input(schemaValidator)
@@ -31,20 +30,4 @@ export const authRouter = router({
       });
       return { success: true, sentToEmail: email };
     }),
-
-    verifyEmail: publicProcedure
-    .input(z.object({token: z.string() }))
-    .query( async ({input})=>{
-      const {token} = input
-      const payload = await getPayloadClient()
-
-      const isVerified = await payload.verifyEmail({
-        collection: "users",
-        token,
-      })
-      if(!isVerified)
-        throw new TRPCError({code: "UNAUTHORIZED"})
-
-        return {sucess: true}
-    })
 });
