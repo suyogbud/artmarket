182846c34afa3ac7e85637b8124f40653081c162
Suyog Singh Budhathoki <suyogbc10@gmail.com>
2024-05-14 13:58:46 -0400

    Added Verification UI

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,7 +1,10 @@
 "use client"
 
 import { trpc } from "@/trpc/client";
-import { XCircle } from "lucide-react";
+import { Loader2, XCircle } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
 
 interface VerifyEmailProps{
     token: string
@@ -12,7 +15,7 @@ const VerifyEmail = ({token}:VerifyEmailProps) => {
         token,
     })
 
-    if(true){
+    if(isError){
         return <div className="flex flex-col items-center gap-2">
             <XCircle className="h-8 w-8 text-red-600"/>
             <h3 className="font-semibold text-xl">There was a problem</h3>
@@ -21,6 +24,32 @@ const VerifyEmail = ({token}:VerifyEmailProps) => {
                 Please try again.
             </p>
         </div>
+    }
+    if(data?.sucess){
+        return(
+            <div className="flex h-full flex-col items-center justify-center">
+                <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+                    <Image src="/email.png" fill alt="Email sent!!"/>
+                </div>
+
+                <h3 className="font-semibold text-2xl">
+                    You&apos;re all set!
+                </h3>
+                <p className="text-muted-foreground text-center mt-1">
+                    Thank you for verifying your email.
+                </p>
+                <Link className={buttonVariants({className: 'mt-4'})} href='/sign-in'>Sign in</Link>
+            </div>
+        )
+    }
+    if(isLoading){
+        return(
+            <div className="flex flex-col items-center gap-2">
+                <Loader2 className="animate-spin h-8 w-8 text-zinc-300"/>
+                <h3 className="font-semibold text-xl">Verifying...</h3>
+                <p className="text-muted-foreground text-sm">This won&apos;t take long.</p>
+            </div>
+        )
     }
 }
  
