7a45838f8b48ed92c4dd364e49cc0b5acf33a7d5
Suyog Singh Budhathoki <suyogbc10@gmail.com>
2024-06-10 12:52:33 -0400

    Edited dashboard typo

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,35 +1,42 @@
-"use client"
+"use client";
 
 import { Button } from "./ui/button";
 import { User } from "@/payload.types";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import Link from "next/link";
 import { useAuth } from "@/hooks/use-auth";
 
-const UserAccountNav = ({user}:{user:User}) => {
-    const {signOut} = useAuth();
-    return (
+const UserAccountNav = ({ user }: { user: User }) => {
+  const { signOut } = useAuth();
+  return (
     <DropdownMenu>
-        <DropdownMenuTrigger asChild className="overflow-visible">
-            <Button size="sm" variant="ghost">My account</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="bg-white w-60 " align='end'>
-            <div className="flex items-center justify-start gap-2 p-2">
-                <div className="flex flex-col space-y-0.5 leading-none">
-                    <p className="font-medium text-sm text-black">
-                        {user.email}
-                    </p>
-                </div>
-            </div>
-            <DropdownMenuSeparator/>
-            <DropdownMenuItem asChild>
-                <Link href="/sell">Seller Dashboard</Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer" onClick={signOut}>
-                Logout
-            </DropdownMenuItem>
-        </DropdownMenuContent>
-    </DropdownMenu>)
-}
- 
+      <DropdownMenuTrigger asChild className="overflow-visible">
+        <Button size="sm" variant="ghost">
+          My account
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white w-60 " align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-0.5 leading-none">
+            <p className="font-medium text-sm text-black">{user.email}</p>
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/merchant">Seller Dashboard</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer" onClick={signOut}>
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
 export default UserAccountNav;
