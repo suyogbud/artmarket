6267a52a97489129af83141a4e577e523779926f
Suyog <suyogbc10@gmail.com>
2024-06-30 22:44:25 -0400

    Added component for collections on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Image from "next/image";
@@ -45,6 +46,7 @@ export default function Home() {
             <Button variant="ghost">Our policy &rarr;</Button>
           </div>
         </div>
+        <ProductReel title="Brand new" href="/products" />
       </MaxWidthWrapper>
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper>
